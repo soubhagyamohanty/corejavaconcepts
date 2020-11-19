0fce3028fba6c8531a59e747f93375389c5dfe62
soubh <soubh@Soubhagya>
2020-11-19 16:54:30 +0530

    5th Commit

---
+++ DataTypes/src/DataTypesDemo.java
@@ -0,0 +1,20 @@
+
+public class DataTypesDemo {
+
+	public static void main(String[] args) {
+
+		byte a = -40;
+		short b = -150;
+		int c = -1000;
+		long d = 100000;
+		char ch ='A';
+		
+		float f = 1.23f;
+		double g = 12345.6789;
+		
+		byte h = 20;
+		
+		byte result = (byte) (a+h);
+	}
+
+}
---
+++ DataTypes/src/ExplicitCast.java
@@ -0,0 +1,15 @@
+
+public class ExplicitCast {
+
+	public static void main(String[] args) {
+		
+		int i =100;
+		byte b=(byte) i;
+		
+		int x=97;
+		char ch =(char) x;
+		System.out.println(ch);
+
+	}
+
+}
